36e3b6da0ab5eb773116631382ce47268b8668cb
MattyHD0 <mattyyorio@gmail.com>
2025-11-29 18:25:12 -0300

    added hysync page

---
+++ hysync/script.js
@@ -0,0 +1,7 @@
+function openPatreon(){
+    window.open("https://www.patreon.com/es-419", '_blank');
+}
+
+function inviteBot(){
+    window.open("https://discord.com/oauth2/authorize?client_id=1290343860962066487&permissions=8&integration_type=0&scope=bot", '_blank');
+}
